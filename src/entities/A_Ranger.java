07dda014146048f02b9f98e6d0110014e85b2738
Sawyer Zock <darthsawyer@eagles.ewu.edu>
2016-05-06 02:52:29 -0700

    Added choice functionality to A_ranger
    You can now type 1 to attack

--- src/entities/A_Ranger.java
+++ src/entities/A_Ranger.java
@@ -1,6 +1,13 @@
 package entities;
 
-public abstract class A_Ranger 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import encounter.RollManager;
+
+public abstract class A_Ranger
 {
 	protected int health;
 	protected int attackMod;
@@ -8,16 +15,51 @@ public abstract class A_Ranger
 	protected String dmgDice;
 	protected int dmgReduction;
 	protected int armorClass;
-	
+
 	public abstract int getHealth();
+
 	public abstract int getAtkMod();
+
 	public abstract int getDmgMod();
+
 	public abstract String getDmgDice();
+
 	public abstract int getDmgReduction();
+
 	public abstract int getArmorClass();
-   public void turn()
-   {
-      System.out.println("Enter your actions etc ");
-   }
+
+	public void turn()
+	{
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		int choice = -1;
+		do
+		{
+			System.out.println("Type 1 to attack: ");
+
+			try
+			{
+				choice = Integer.parseInt(input.readLine());
+			}
+			catch (NumberFormatException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		while (choice != 1);
+		
+		
+		if (choice == 1)
+		{
+			System.out.printf("You attack with a(n) %d, dealing %d damage!\n\n", RollManager.getInstance().roll("1d20+" + getAtkMod()), RollManager.getInstance().roll("" + getDmgDice() + "+" + getDmgMod()));
+		}
+		
+		
+	}
 
 }
